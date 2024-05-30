dc69dbf1a89e3d6df6c5ed6bdbfb1284348d4177
Tobias Brandt <>
2024-05-30 17:12:28 +0200

    Webcam stream erstellt

---
+++ Website/webcam_stream/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+import cv2
+from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
+import threading
+
+class VideoCamera:
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        _, jpeg = cv2.imencode('.jpg', self.frame)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip.gzip_page
+def livefe(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
