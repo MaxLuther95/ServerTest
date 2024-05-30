a3e9ce4d227f4d6e2d23a1ec64661d258bc20181
Tobias Brandt <>
2024-05-30 17:55:11 +0200

    Erstelle die Webcam instanz global und kommentieren des Programmes

--- Website/webcam_stream/views.py
+++ Website/webcam_stream/views.py
@@ -4,33 +4,44 @@ from django.http import StreamingHttpResponse
 from django.views.decorators import gzip
 import threading
 
+# Erstelle eine globale Instanz der VideoCamera-Klasse
+global_cam = VideoCamera()
+
 class VideoCamera:
     def __init__(self):
+        # Initialisierung der Webcam
         self.video = cv2.VideoCapture(0)
+        # Erfassen des ersten Frames
         (self.grabbed, self.frame) = self.video.read()
+        # Starten eines Threads, um den Frame kontinuierlich zu aktualisieren
         threading.Thread(target=self.update, args=()).start()
 
     def __del__(self):
+        # Freigabe der Webcam-Ressourcen beim Beenden
         self.video.release()
 
     def get_frame(self):
+        # Erfassen des aktuellen Frames und Kodieren als JPEG
         _, jpeg = cv2.imencode('.jpg', self.frame)
         return jpeg.tobytes()
 
     def update(self):
         while True:
+            # Aktualisierung des Frames
             (self.grabbed, self.frame) = self.video.read()
 
 def gen(camera):
     while True:
+        # Generieren des Streams für die Live-Übertragung
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+# Dekorator, der die Antwort komprimiert, bevor sie an den Client gesendet wird
 @gzip.gzip_page
 def livefe(request):
     try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+        # Verwende die globale Instanz der VideoCamera-Klasse
+        return StreamingHttpResponse(gen(global_cam), content_type="multipart/x-mixed-replace;boundary=frame")
     except:
         pass
