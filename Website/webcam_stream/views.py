a05a7392e3f14c3db9e3427d5c9793a44d3f4f0f
Max Luther <->
2024-06-05 18:04:35 +0200

    ändern der Kamera von ID zu Name

--- Website/webcam_stream/views.py
+++ Website/webcam_stream/views.py
@@ -4,14 +4,18 @@ from django.http import StreamingHttpResponse
 
 def capture_video_from_cam():
     # Initialisiere den VideoCapture nur einmal, um Konflikte zu vermeiden
-    cap = cv2.VideoCapture(0)  # 0 steht für die erste angeschlossene Kamera
+    caps = []
+    caps.append(cv2.VideoCapture(filename="/dev/video0"))
+    #caps.append(cv2.VideoCapture(filename="/dev/video1")) #nicht möglich
+    #caps.append(cv2.VideoCapture(filename="/dev/media3")) #nicht möglich
 
     while True:
-        ret, frame = cap.read()
-        frame = cv2.flip(frame, 1)  # Spiegelt das aktuelle Frame horizontal
-        _, jpeg = cv2.imencode('.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+        for cap in caps:
+            _, frame = cap.read()
+            frame = cv2.flip(frame, 1) # Spiegelt das aktuelle Frame horizontal
+            _, jpeg = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 def show_video_on_page(request):
     return StreamingHttpResponse(capture_video_from_cam(), content_type="multipart/x-mixed-replace;boundary=frame")
