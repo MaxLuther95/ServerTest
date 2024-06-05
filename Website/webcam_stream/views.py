18ca5c19002086c233c9544ae559704f25dcd9b9
Tobias Brandt <>
2024-06-05 18:32:42 +0200

    Video Kamera als Klasse auslagern und ansteuern

--- Website/webcam_stream/views.py
+++ Website/webcam_stream/views.py
@@ -1,21 +1,38 @@
 # In deiner views.py
 import cv2
 from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
+import threading
 
-def capture_video_from_cam():
-    # Initialisiere den VideoCapture nur einmal, um Konflikte zu vermeiden
-    caps = []
-    caps.append(cv2.VideoCapture(filename="/dev/video0"))
-    #caps.append(cv2.VideoCapture(filename="/dev/video1")) #nicht möglich
-    #caps.append(cv2.VideoCapture(filename="/dev/media3")) #nicht möglich
+@gzip.gzip_page
+def Home(request):
+    try:
+        return StreamingHttpResponse(gen(cam), content_type = "multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
 
+class VideoCamera(object):
+    def __init__(self, cam_filename):
+        self.video = cv2.VideoCapture(filename = cam_filename)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target = self.update, args = ()).start()
+    
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+def gen(camera: VideoCamera):
     while True:
-        for cap in caps:
-            _, frame = cap.read()
-            frame = cv2.flip(frame, 1) # Spiegelt das aktuelle Frame horizontal
-            _, jpeg = cv2.imencode('.jpg', frame)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-def show_video_on_page(request):
-    return StreamingHttpResponse(capture_video_from_cam(), content_type="multipart/x-mixed-replace;boundary=frame")
+cam = VideoCamera('/dev/video0')
